85614481698123690de305e94f67ca7fa8b20dad
saidsoud72 <said.omar@student.moringaschool.com>
2022-08-25 14:10:51 +0300

    done assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+shout('hello');
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+whisper('HELLO');
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+logShout('hello');
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+logWhisper('HELLO');
+
+function sayHiToHeadphonedRoommate(string){
+    if(string===string.toLowerCase()){
+        return "I can't hear you!" 
+    }
+    if(string===string.toUpperCase()){
+        return "YES INDEED!" 
+    }
+    if(string==="Let's have dinner together!"){
+        return "I would love to!" 
+    }
+}
+
+sayHiToHeadphonedRoommate('hello');
+sayHiToHeadphonedRoommate('HELLO');
+sayHiToHeadphonedRoommate("Let's have dinner together!");
